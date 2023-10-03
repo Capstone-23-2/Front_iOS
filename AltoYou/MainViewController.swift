63e138abaea351a42f2338021fa701a356e28068
JEONG-J <ikejhc159@gmail.com>
2023-10-03 17:14:20 +0900

    Feat : 깃 연결

---
+++ AltoYou/MainViewController.swift
@@ -0,0 +1,29 @@
+//
+//  MainViewController.swift
+//  AltoYou
+//
+//  Created by 정의찬 on 10/3/23.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+    // MARK: - init
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        self.view.backgroundColor = .blue
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
